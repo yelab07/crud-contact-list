56f1432e29b75b45fabd069410351a568fd15e53
yelab07 <79489573+yelab07@users.noreply.github.com>
2021-10-06 02:38:31 -0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Title from './component/Text';
+import { useState } from "react";
 function App() {
+  const [form, setForm] = useState({ name: "", phone: "", email: "" });
+  const [contacts, setContacts] = useState([]);
+
+  const onChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const addToContacts = (e) => {
+    e.preventDefault();
+  }
   return (
     <div className="App">
-      
+      <input onChange={onchange} name="name" type="text" />
+      <input onChange={onchange} name="phone" type="text" />
+      <input onChange={onchange} name="email" type="text" />
+      <button> Submit</button>
     </div>
   );
 }
